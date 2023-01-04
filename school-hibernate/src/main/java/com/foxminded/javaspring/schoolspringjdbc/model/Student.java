fd0747e30af64a137e5156c6e1cb61028059e690
Asus <Asus@DESKTOP-5NL9HPH>
2023-01-04 14:43:55 +0200

    Many-to-one

--- school-hibernate/src/main/java/com/foxminded/javaspring/schoolspringjdbc/model/Student.java
+++ school-hibernate/src/main/java/com/foxminded/javaspring/schoolspringjdbc/model/Student.java
@@ -2,17 +2,37 @@ package com.foxminded.javaspring.schoolspringjdbc.model;
 
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.*;
 
 @Data
 @NoArgsConstructor
-
+@Entity
+@Table(name = "students")
 public class Student {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int studentID;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
 	private int groupID;
+	
+	@Column(name = "FIRST_NAME")
 	private String firstName;
+	
+	@Column(name = "LAST_NAME")
 	private String lastName;
+	
+	@Column(name = "COURSES")
 	private List<Course> courses;
 
 	public Student(int studentID, String firstName, String lastName) {
